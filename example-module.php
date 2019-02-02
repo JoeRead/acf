1565afd5678e89a30beffae0f566e5f322b26569
Joe Read <joe.read2@gmail.com>
2019-02-02 10:34:21 +0000

    Create example-module.php

---
+++ example-module.php
@@ -0,0 +1,31 @@
+<?php
+/*  Your Module Title/Description Goes Here  -------
+
+//Update the below section as you create your ACF fields - this helps to keep track of their identifier and the type of field.
+
+   example-module
+    example_heading - text
+    example_content - textarea
+    example_image1 - image (array)
+
+*/
+
+// This section turns your ACF value into a string - this makes it easier to manipuulate and display as you create your module.
+// Update this using the values above
+
+$heading = get_sub_field('example_heading');
+$content = get_sub_field('example_content');
+$img1 = get_sub_field('sexample_image1');
+  $img1_url = $img1['url'];
+  $img1_alt = $img1['alt'];
+
+
+?>
+
+<div id="my-example-row" class="flr100">
+  <div class="container">
+  <h1><?php echo $heading; ?></h1>
+  <h1><?php echo $heading; ?></h1>
+  </div>
+</div>
+
