2316483138e925e6c34f6e6f64851bdfcc03e6d5
Joe Read <joe.read2@gmail.com>
2019-04-30 16:38:14 +0100

    Create page-template-fields.php

---
+++ page-template-fields.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template Name: [template name]
+ *
+ * @package understrap
+ */
+
+get_header();
+
+?>
+
+<div id="full-width-page-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 content-area" id="primary">
+
+				<main class="site-main" id="main" role="main">
+					<?php the_title() ?>
+					
+
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+
+
+						<?php
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+
+							comments_template();
+
+						endif;
+						?>
+
+					<?php endwhile; // end of the loop. ?>
+
+				</main><!-- #main -->
+
+			</div><!-- #primary -->
+
+		</div><!-- .row end -->
+
+	</div><!-- Container end -->
+
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
